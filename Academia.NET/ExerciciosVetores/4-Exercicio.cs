6baeb9b7d8faeaf771094766d3cfc06ee78e8f2c
Cayan Mello <cayanneves@gmail.com>
2023-10-09 15:46:54 -0300

    Exercicio 4 feito

--- Academia.NET/ExerciciosVetores/4-Exercicio.cs
+++ Academia.NET/ExerciciosVetores/4-Exercicio.cs
@@ -10,7 +10,41 @@ namespace Academia.NET.ExerciciosVetores
     {
         public static void vetores()
         {
+            //4.Escreva um algoritmo que leia dois vetores de 10 posições e faça a multiplicação dos elementos de mesmo índice,
+            //    colocando o resultado em um terceiro vetor. Mostre o vetor resultante.
 
+            int tamanho = 10;
+            int[] vetor1 = new int[tamanho];
+            int[] vetor2 = new int[tamanho];
+            int[] resultado = new int[tamanho];
+
+            Console.WriteLine("Digite 10 números para o primeiro vetor");
+
+            for (int i = 0; i < vetor1.Length; i++)
+            {
+                Console.Write($"Digite o {i + 1}º número: ");
+                vetor1[i] = int.Parse(Console.ReadLine());
+            }
+
+            Console.WriteLine("Digite 10 números para o segundo vetor");
+
+
+            for (int i = 0; i < vetor2.Length; i++)
+            {
+                Console.Write($"Digite o {i + 1}º número: ");
+                vetor2[i] = int.Parse(Console.ReadLine());
+            }
+
+            for (int i = 0; i < resultado.Length; i++)
+            {
+                resultado[i] = vetor1[i] * vetor2[i];
+            }
+
+            Console.WriteLine("Resultado da soma dos vetores:");
+            for (int i = 0; i < resultado.Length; i++)
+            {
+                Console.WriteLine($"Elemento {i + 1}: {resultado[i]}");
+            }
         }
     }
 }
