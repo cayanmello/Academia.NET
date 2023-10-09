7361b89eee325a757727a05dc185817add20937b
Cayan Mello <cayanneves@gmail.com>
2023-10-09 16:28:00 -0300

    Exercicio 9 feito

--- Academia.NET/ExerciciosVetores/9-Exercicio.cs
+++ Academia.NET/ExerciciosVetores/9-Exercicio.cs
@@ -10,7 +10,28 @@ namespace Academia.NET.ExerciciosVetores
     {
         public static void vetores()
         {
+            //9.Escreva um algoritmo que leia os valores para um vetor de 10 elementos, e em seguida ordene em ordem crescente os valores desse vetor, utilizando um vetor auxiliar.
 
+            int tamanho = 10;
+            int[] vetor = new int[tamanho];
+            int[] vetorOrdenado = new int[tamanho];
+
+            for (int i = 0; i < tamanho; i++)
+            {
+                Console.Write($"Digite o valor para a posição {i + 1}: ");
+                vetor[i] = int.Parse(Console.ReadLine());
+            }
+
+            Array.Copy(vetor, vetorOrdenado, tamanho);
+            Array.Sort(vetorOrdenado);
+
+            Console.WriteLine("Vetor Ordenado em Ordem Crescente:");
+            foreach (var num in vetorOrdenado)
+            {
+                Console.Write($"{num} ");
+            }
+
+            Console.ReadLine();
         }
     }
 }
