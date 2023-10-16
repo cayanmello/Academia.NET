7e74f28a278b2c3f056c22564ae2414be56fe959
Cayan Mello <cayanneves@gmail.com>
2023-10-16 16:13:35 -0300

    Exercicio 12 da matriz feito

--- Academia.NET/ExerciciosMatrizes/12-Exercicio.cs
+++ Academia.NET/ExerciciosMatrizes/12-Exercicio.cs
@@ -13,7 +13,31 @@ namespace Academia.NET.ExerciciosMatrizes
         {
             //12) Escreva um programa que leia os valores de uma matriz 4x3, e em seguida mostre na tela apenas os valores cuja soma dos índices(i + j) seja um número par.
 
-           
+            int linhas = 4;
+            int colunas = 3;
+            int[,] matriz = new int[linhas, colunas];
+
+            Console.WriteLine($"Digite os valores da matriz {linhas}x{colunas}:");
+            for (int i = 0; i < linhas; ++i)
+            {
+                for (int j = 0; j < colunas; ++j)
+                {
+                    Console.Write($"Elemento [{i},{j}]: ");
+                    matriz[i, j] = Convert.ToInt32(Console.ReadLine());
+                }
+            }
+
+            Console.WriteLine($"Valores cuja soma dos índices (i+j) é um número par na matriz {linhas}x{colunas}:");
+            for (int i = 0; i < linhas; ++i)
+            {
+                for (int j = 0; j < colunas; ++j)
+                {
+                    if ((i + j) % 2 == 0)
+                    {
+                        Console.WriteLine($"Elemento [{i},{j}]: {matriz[i, j]}");
+                    }
+                }
+            }
         }
     }
 }
--- Academia.NET/Program.cs
+++ Academia.NET/Program.cs
@@ -16,6 +16,6 @@ internal class Program
         //exercicioLoop2._1_Exercicio.loop2();
         //exercicioSequenciais._1_Exercicio.sequenciais();
         //exercicioVetores._1_Exercicio.vetores();
-        exercicioMatrizes._17_Exercicio.matrizes();
+        //exercicioMatrizes._1_Exercicio.matrizes();
     }
 }
