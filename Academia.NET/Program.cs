2681517f00787626fff9884eddce552357e95787
Cayan Mello <cayanneves@gmail.com>
2023-10-16 13:18:06 -0300

    Exercicio 6 da matriz feito

--- Academia.NET/Program.cs
+++ Academia.NET/Program.cs
@@ -16,6 +16,6 @@ internal class Program
         //exercicioLoop2._1_Exercicio.loop2();
         //exercicioSequenciais._1_Exercicio.sequenciais();
         //exercicioVetores._1_Exercicio.vetores();
-        exercicioMatrizes._5_Exercicio.matrizes();
+        exercicioMatrizes._6_Exercicio.matrizes();
     }
 }
