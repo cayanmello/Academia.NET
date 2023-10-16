2880601b9024235564a1ac462c94bc89ef552ea8
Cayan Mello <cayanneves@gmail.com>
2023-10-16 15:47:21 -0300

    Exercicio 13 da matriz feito

--- Academia.NET/ExerciciosMatrizes/12-Exercicio.cs
+++ Academia.NET/ExerciciosMatrizes/12-Exercicio.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.Intrinsics.X86;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,7 +11,9 @@ namespace Academia.NET.ExerciciosMatrizes
     {
         public static void matrizes()
         {
+            //12) Escreva um programa que leia os valores de uma matriz 4x3, e em seguida mostre na tela apenas os valores cuja soma dos índices(i + j) seja um número par.
 
+           
         }
     }
 }
--- Academia.NET/ExerciciosMatrizes/13-Exercicio.cs
+++ Academia.NET/ExerciciosMatrizes/13-Exercicio.cs
@@ -10,7 +10,37 @@ namespace Academia.NET.ExerciciosMatrizes
     {
         public static void matrizes()
         {
+            //13) Escreva um programa que leia uma matriz de ordem 5(ou seja, 5x5) e verifique se a soma dos elementos da diagonal principal é igual a soma dos elementos da
+            //diagonal secundária.
 
+            int[,] matriz = new int[5, 5] {
+                {1, 2, 3, 4, 5},
+                {6, 7, 8, 9, 10},
+                {11, 12, 13, 14, 15},
+                {16, 17, 18, 19, 20},
+                {21, 22, 23, 24, 25}
+            };
+
+            int somaDiagonalPrincipal = 0;
+            for (int i = 0; i < 5; ++i)
+            {
+                somaDiagonalPrincipal += matriz[i, i];
+            }
+
+            int somaDiagonalSecundaria = 0;
+            for (int i = 0; i < 5; ++i)
+            {
+                somaDiagonalSecundaria += matriz[i, 4 - i];
+            }
+
+            if (somaDiagonalPrincipal == somaDiagonalSecundaria)
+            {
+                Console.WriteLine($"A soma dos elementos da diagonal principal ({somaDiagonalPrincipal}) é igual à soma dos elementos da diagonal secundária ({somaDiagonalSecundaria}).");
+            }
+            else
+            {
+                Console.WriteLine($"A soma dos elementos da diagonal principal ({somaDiagonalPrincipal}) não é igual à soma dos elementos da diagonal secundária ({somaDiagonalSecundaria}).");
+            }
         }
     }
 }
