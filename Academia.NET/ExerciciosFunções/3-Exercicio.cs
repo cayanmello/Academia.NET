49e759791fd418b04cf25f5f9e75d9a53d4f1ea5
Cayan Mello <cayanneves@gmail.com>
2023-10-18 15:56:54 -0300

    Exercicio 3 de Funções feita

--- Academia.NET/ExerciciosFunções/3-Exercicio.cs
+++ Academia.NET/ExerciciosFunções/3-Exercicio.cs
@@ -10,7 +10,19 @@ namespace Academia.NET.ExerciciosFunções
     {
         public static void funcoes()
         {
-            
+            //3) Desenvolva uma função que calcule a média de três números fornecidos como argumentos.
+
+            Console.WriteLine("Digite três números: ");
+            double a = int.Parse(Console.ReadLine());
+            double b = int.Parse(Console.ReadLine());
+            double c = int.Parse(Console.ReadLine());
+            double resultado = media(a, b, c);
+            Console.WriteLine($"A média dos três números são: {resultado}");
+
+            static double media(double a, double b, double c)
+            {
+                return (a + b + c) / 3;
+            }
         }
     }
 }
