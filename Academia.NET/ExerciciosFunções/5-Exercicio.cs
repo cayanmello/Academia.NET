c2eff01f8ff7daa9efdc0bdb7b8d58289bd35a59
Cayan Mello <cayanneves@gmail.com>
2023-10-18 16:54:13 -0300

    Exercicio 5 de Funções feita

--- Academia.NET/ExerciciosFunções/5-Exercicio.cs
+++ Academia.NET/ExerciciosFunções/5-Exercicio.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace Academia.NET.ExerciciosFunções
@@ -10,8 +11,18 @@ namespace Academia.NET.ExerciciosFunções
     {
         public static void funcoes()
         {
+            //5) Desenvolva uma função que converta uma temperatura em graus Celsius para Fahrenheit.
 
+            Console.Write("Digite a temperatura em graus Celsius: ");
+            int tempCelsius = int.Parse(Console.ReadLine());
+            double tempFahrenheit = ConverterParaFahrenheit(tempCelsius);
+            Console.WriteLine($"{tempCelsius} graus Celsius são {tempFahrenheit} graus Fahrenheit.");
+        }
 
+        static double ConverterParaFahrenheit(double tempCelsius)
+        {
+            double temperaturaFahrenheit = tempCelsius * 9 / 5 + 32;
+            return temperaturaFahrenheit;
         }
     }
 }
