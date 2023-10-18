7aea903a8ba9c25b25646934f60bb58609c447d5
Cayan Mello <cayanneves@gmail.com>
2023-10-18 16:49:21 -0300

    Exercicio 4 de Funções feita

--- Academia.NET/ExerciciosFunções/4-Exercicio.cs
+++ Academia.NET/ExerciciosFunções/4-Exercicio.cs
@@ -10,8 +10,30 @@ namespace Academia.NET.ExerciciosFunções
     {
         public static void funcoes()
         {
+            //4) Escreva uma função que calcule o fatorial de um número inteiro positivo.
 
+            Console.Write("Digite um número inteiro positivo: ");
+            int numero = int.Parse(Console.ReadLine());
 
+            long resultado = CalcularFatorial(numero);
+            Console.WriteLine($"O fatorial de {numero} é {resultado}");
+        }
+
+        static long CalcularFatorial(int numero)
+        {
+            if (numero == 0 || numero == 1)
+            {
+                return 1;
+            }
+            else
+            {
+                long fatorial = 1;
+                for (int i = 2; i <= numero; i++)
+                {
+                    fatorial *= i;
+                }
+                return fatorial;
+            }
         }
     }
 }
---
+++ Academia.NET/ExerciciosFunções/Desafio.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Academia.NET.ExerciciosFunções
+{
+    internal class _10_Exercicio
+    {
+        public static void funcoes()
+        {
+
+
+        }
+    }
+}
