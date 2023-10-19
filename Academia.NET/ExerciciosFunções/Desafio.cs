84509e2713b5f45c72138d29a062830cdadc688d
Cayan Mello <cayanneves@gmail.com>
2023-10-19 10:48:31 -0300

    Desafio de Funções feita

--- Academia.NET/ExerciciosFunções/Desafio.cs
+++ Academia.NET/ExerciciosFunções/Desafio.cs
@@ -15,6 +15,37 @@ namespace Academia.NET.ExerciciosFunções
         {
             //Ler um vetor de inteiros com 10 elementos e passar por parâmetro para uma função que verifica e retorna o menor elemento do vetor.
 
+
+            static int EncontrarMenorElemento(int[] vetor)
+            {
+                int menor = vetor[0];
+
+            for (int i = 1; i < vetor.Length; ++i)
+            {
+                if (vetor[i] < menor)
+                {
+                    menor = vetor[i];
+                }
+            }
+
+            return menor;
+        }
+            menorElemento();
+
+            static void menorElemento()
+            {
+                int[] vetor = new int[10];
+
+                Console.WriteLine("Digite 10 numeros inteiros para preencher o vetor:");
+                for (int i = 0; i < 10; ++i)
+                {
+                    vetor[i] = int.Parse(Console.ReadLine());
+                }
+
+                int menorElemento = EncontrarMenorElemento(vetor);
+
+                Console.WriteLine("O menor elemento no vetor é: " + menorElemento);
+            }
         }
     }
 }
