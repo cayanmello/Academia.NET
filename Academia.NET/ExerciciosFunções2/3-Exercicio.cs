72cda3c19551351e0a0ce505a9388606df37d318
Cayan Mello <cayanneves@gmail.com>
2023-10-19 15:00:35 -0300

    Exercicio 3 da segunda lista de Funções feita

--- Academia.NET/ExerciciosFunções2/3-Exercicio.cs
+++ Academia.NET/ExerciciosFunções2/3-Exercicio.cs
@@ -12,7 +12,51 @@ namespace Academia.NET.ExerciciosFunções2
         {
             //3 - Escreva uma função chamada TransporMatriz que recebe uma matriz como parâmetro e retorna a matriz transposta.
 
+            int[,] matriz = {
+            {1, 2, 3},
+            {4, 5, 6},
+            {7, 8, 9}
+        };
 
+            int[,] matrizTransposta = TransporMatriz(matriz);
+
+            Console.WriteLine("Matriz Original:");
+            ImprimirMatriz(matriz);
+
+            Console.WriteLine("\nMatriz Transposta:");
+            ImprimirMatriz(matrizTransposta);
+        }
+
+        static int[,] TransporMatriz(int[,] matriz)
+        {
+            int linhas = matriz.GetLength(0);
+            int colunas = matriz.GetLength(1);
+            int[,] matrizTransposta = new int[colunas, linhas];
+
+            for (int i = 0; i < linhas; i++)
+            {
+                for (int j = 0; j < colunas; j++)
+                {
+                    matrizTransposta[j, i] = matriz[i, j];
+                }
+            }
+
+            return matrizTransposta;
+        }
+
+        static void ImprimirMatriz(int[,] matriz)
+        {
+            int linhas = matriz.GetLength(0);
+            int colunas = matriz.GetLength(1);
+
+            for (int i = 0; i < linhas; i++)
+            {
+                for (int j = 0; j < colunas; j++)
+                {
+                    Console.Write(matriz[i, j] + "\t");
+                }
+                Console.WriteLine();
+            }
         }
     }
 }
