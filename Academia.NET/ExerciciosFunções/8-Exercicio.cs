d49a64f2bbc754a42242be3b963ccb3f4bb9446b
Cayan Mello <cayanneves@gmail.com>
2023-10-19 10:34:02 -0300

    Exercicio 8 de Funções feita

--- Academia.NET/ExerciciosFunções/8-Exercicio.cs
+++ Academia.NET/ExerciciosFunções/8-Exercicio.cs
@@ -13,7 +13,33 @@ namespace Academia.NET.ExerciciosFunções
         {
             //8) Ler um número inteiro e passar por parâmetro para uma função e 0, se o número é par, ou 1, se o número é ímpar.
 
+            static int VerificarParImpar(int numero)
+            {
+                if (numero % 2 == 0)
+                {
+                    return 0;
+                }
+                else
+                {
+                    return 1;
+                }
+            }
 
-        }
+            static void lerImparPar()
+            {
+                Console.Write("Digite um número: ");
+                int numero = int.Parse(Console.ReadLine());
+
+                int resultado = VerificarParImpar(numero);
+
+                if (resultado == 0)
+                {
+                    Console.WriteLine(numero + " é um número par.");
+                }
+                else
+                {
+                    Console.WriteLine(numero + " é um número ímpar.");
+                }
+            }
     }
 }
