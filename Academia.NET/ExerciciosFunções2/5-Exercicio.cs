1ce48aa77d940d026f663787aa98f5298f98c64e
Cayan Mello <cayanneves@gmail.com>
2023-10-19 16:31:06 -0300

    Exercicio 5 da segunda lista de Funções feita

--- Academia.NET/ExerciciosFunções2/5-Exercicio.cs
+++ Academia.NET/ExerciciosFunções2/5-Exercicio.cs
@@ -14,6 +14,45 @@ namespace Academia.NET.ExerciciosFunções2
             //5 - Crie uma função chamada SomaDiagonalPrincipal que recebe uma matriz quadrada como parâmetro e retorna a soma dos elementos na diagonal principal.
 
 
+            int[,] matriz = {
+                {1, 2, 3},
+                {4, 5, 6},
+                {7, 8, 9}
+            };
+
+            int somaDiagonalPrincipal = SomaDiagonalPrincipal(matriz);
+
+            Console.WriteLine("Matriz:");
+            ImprimirMatriz(matriz);
+
+            Console.WriteLine($"Soma da Diagonal Principal: {somaDiagonalPrincipal}");
+        }
+
+        static int SomaDiagonalPrincipal(int[,] matriz)
+        {
+            int tamanho = matriz.GetLength(0);
+            int soma = 0;
+
+            for (int i = 0; i < tamanho; i++)
+            {
+                soma += matriz[i, i];
+            }
+
+            return soma;
+        }
+
+        static void ImprimirMatriz(int[,] matriz)
+        {
+            int tamanho = matriz.GetLength(0);
+
+            for (int i = 0; i < tamanho; i++)
+            {
+                for (int j = 0; j < tamanho; j++)
+                {
+                    Console.Write(matriz[i, j] + "\t");
+                }
+                Console.WriteLine();
+            }
         }
     }
 }
